1c4ca025a0af7a64213504f99d2e207031688402
Felipe Milioni <felipemilioni@hotmail.com>
2019-11-06 15:44:32 -0300

    build

--- dist/components/Circle.js
+++ dist/components/Circle.js
@@ -102,7 +102,7 @@
     if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
   }
 
-  var evtNames = ['click', 'mouseout', 'mouseover'];
+  var evtNames = ['click', 'mouseout', 'mouseover', 'dragend'];
 
   var wrappedPromise = function wrappedPromise() {
     var wrappedPromise = {},
